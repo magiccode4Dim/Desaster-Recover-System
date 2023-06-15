0dc74574aeb4c795a6b2d70cbf4ad64edf8f8bdf
magiccode4Dim <128492329+magiccode4Dim@users.noreply.github.com>
2023-06-15 22:13:42 +0200

    Estrutura da API iniciada

---
+++ DRS_API/DRS_EUREKA_SERVER/src/main/java/com/magiccode4dim/drs_eureka_server/EurekaServer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.magiccode4dim.drs_eureka_server;
+
+/**
+ *
+ * @author narci
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+     public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        SpringApplication.run(EurekaServer.class,args);
+        //mvn spring-boot:run
+
+        
+    }
+}
