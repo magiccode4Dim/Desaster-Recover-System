89325ae256155a5b8a406429ff70f6ff882ce831
Narciso Cadeado <narcisopascoal97@gmail.com>
2023-07-06 12:51:53 +0200

    Sistema de Recuperacao

---
+++ DRS_API/STATUSSERVICE/src/main/java/com/magiccode4dim/statusservice/statussql.sql
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  narciso
+ * Created: Jul 5, 2023
+ */
+
+
+create table status(id int primary key auto_increment, server_addrs varchar(700)
+, data_criacao varchar(700),valor varchar(30));
+
+desc status;
