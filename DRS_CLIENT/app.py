7cdbeaeab2b37ee867296e08b6085c59136afdec
Narciso Cadeado <narcisopascoal97@gmail.com>
2023-07-29 17:03:37 +0200

    script de backup

--- DRS_CLIENT/app.py
+++ DRS_CLIENT/app.py
@@ -9,7 +9,7 @@ if __name__=="__main__":
         #get csrf_token
         response =  requests.get("http://127.0.0.1:8000/api/statusservice/sendstatus")
         sta['csrfmiddlewaretoken'] = response.json()["csrf_token"]
-        sta['token'] = 'RiSz5mCTcFeC1dcoFTOA1VV0y67Fg8mu-NLhlgRZD4gb5f53d08fd2e4897865ee5ea7129c955'
+        sta['token'] = 'KC8us4mWDtAMcKWHd8L7b2Comx-pzLwN6b2TCfE4xeU8275012e3d644533b233eb90ed957ad1'
         response =  requests.post("http://127.0.0.1:8000/api/statusservice/sendstatus",data=sta,cookies=response.cookies)
         print(response.json())
         time.sleep(1)
