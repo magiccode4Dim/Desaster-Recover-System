5110feababaf6d0272fe7d691c99d61320b477cc
Narciso Cadeado <narcisopascoal97@gmail.com>
2023-07-30 16:53:49 +0200

    agora o cliente nao fica sempre a mandar requisicoes para o backend

--- DRS_API/BACKUPSERVICE/src/main/java/com/magiccode4dim/backupservice/BackupService.java
+++ DRS_API/BACKUPSERVICE/src/main/java/com/magiccode4dim/backupservice/BackupService.java
@@ -9,7 +9,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
@@ -23,7 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.magiccode4dim.backupservice.requests.Request;
 import com.magiccode4dim.backupservice.requests.Objects.Volume;
 
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -34,29 +32,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 @RequestMapping("/drs/api/backup")
-public class BackupService{
+public class BackupService {
 
     // private final StatusCRUD ss;
-    private final String apiDockerUri; 
+    private final String apiDockerUri;
 
     @Autowired
     public BackupService() {
         this.apiDockerUri = "http://localhost:5001";
     }
-  
-    //volumes
+
+    // volumes
     // criar volume
     @Secured("USER")
     @PostMapping("/volumes/create")
     @ResponseBody
-    public ResponseEntity<String>  createContainer(@RequestBody Volume cont) {
+    public ResponseEntity<String> createContainer(@RequestBody Volume cont) {
         // cria um container
         String url = this.apiDockerUri + "/volumes/create"; // URL de destino
         // Enviar a requisição POST
         ResponseEntity<String> responseEntity = Request.sendPostRequest(cont, null, null, url);
         return responseEntity;
     }
-    //get all volumes
+
+    // get all volumes
     @Secured("USER")
     @GetMapping("/volumes/getall")
     @ResponseBody
@@ -67,18 +66,18 @@ public class BackupService{
         return responseEntity;
 
     }
-    //get volume
+
+    // get volume
     @Secured("USER")
     @GetMapping("/volumes/get/{volumename}")
     @ResponseBody
     public ResponseEntity<String> getVolume(@PathVariable String volumename) {
-        String url = this.apiDockerUri + "/volumes/get/"+volumename; // URL de destino
+        String url = this.apiDockerUri + "/volumes/get/" + volumename; // URL de destino
         // Enviar a requisição POST
         ResponseEntity<String> responseEntity = Request.sendGetRequest(null, null, url);
         return responseEntity;
 
     }
-
 
     // como usar recursos avancados do mongodb
 
